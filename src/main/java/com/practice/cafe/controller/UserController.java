354c5aa69cde717fd24cad941661387f62c1ea11
HyeonJin Song <shwj1602@naver.com>
2024-06-10 19:33:03 +0900

    Feat : 사용자 회원가입 기능 추가

---
+++ src/main/java/com/practice/cafe/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.practice.cafe.controller;
+
+import com.practice.cafe.dto.request.user.SignUpRequestDto;
+import com.practice.cafe.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cafe")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/users/signup")
+    public ResponseEntity<?> signup(@Valid @RequestBody SignUpRequestDto requestDto){
+        return userService.signUp(requestDto);
+    }
+}
