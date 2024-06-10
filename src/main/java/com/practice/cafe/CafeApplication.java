fa1f254d6267c10ac15a229cf6d06eb70d5e2045
HyeonJin Song <shwj1602@naver.com>
2024-06-10 17:34:45 +0900

    Init : init commit

---
+++ src/main/java/com/practice/cafe/CafeApplication.java
@@ -0,0 +1,15 @@
+package com.practice.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class CafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeApplication.class, args);
+	}
+
+}
