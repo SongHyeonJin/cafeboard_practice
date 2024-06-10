1fbd144df0a2ed932f344ea3ccb8b14e6e68277e
HyeonJin Song <shwj1602@naver.com>
2024-06-10 19:30:45 +0900

    Feat : 응답 메시지 관리를 위한 BasicResponseDto, UserStatusResponseDto 추가

---
+++ src/main/java/com/practice/cafe/dto/response/user/UserStatusResponseDto.java
@@ -0,0 +1,12 @@
+package com.practice.cafe.dto.response.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserStatusResponseDto {
+
+    private String name;
+    private String message;
+}
