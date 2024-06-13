1531130b31623d65db32facccc01edf296423607
HyeonJin Song <shwj1602@naver.com>
2024-06-13 21:20:45 +0900

    Feat : 테스트를 위해 UserStatusResponseDto에 회원가입 시간 정보 필드 추가

--- src/main/java/com/practice/cafe/dto/response/user/UserStatusResponseDto.java
+++ src/main/java/com/practice/cafe/dto/response/user/UserStatusResponseDto.java
@@ -3,10 +3,13 @@ package com.practice.cafe.dto.response.user;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class UserStatusResponseDto {
 
     private String name;
     private String message;
+    private LocalDateTime signupDateTime;
 }
