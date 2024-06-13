e8395ef72d0c270dd5a2484a6dabdc1d89d54268
HyeonJin Song <shwj1602@naver.com>
2024-06-13 21:17:30 +0900

    Refactor : ExceptionEnum 클래스에서 USER_DUPLICATION과 NOT_ADMIN_KEY의 메시지 변경

--- src/main/java/com/practice/cafe/dto/error/ExceptionEnum.java
+++ src/main/java/com/practice/cafe/dto/error/ExceptionEnum.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ExceptionEnum {
-    USER_DUPLICATION(HttpStatus.BAD_REQUEST.value(), "중복된 이메일입니다."),
-    NOT_ADMIN_KEY(HttpStatus.BAD_REQUEST.value(), "관리자 암호가 일치하지 않습니다.");
+    USER_DUPLICATION(HttpStatus.BAD_REQUEST.value(), "이미 사용 중인 이메일입니다."),
+    NOT_ADMIN_KEY(HttpStatus.BAD_REQUEST.value(), "관리자 키가 일치하지 않습니다.");
 
 
     private final int status;
