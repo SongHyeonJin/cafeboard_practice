e280e166dd7d0b5f7f66afa1beb947b161ff1b5f
HyeonJin Song <shwj1602@naver.com>
2024-06-17 22:11:55 +0900

    Feat : LoginRequestDto 클래스 추가

---
+++ src/main/java/com/practice/cafe/dto/request/user/LoginRequestDto.java
@@ -0,0 +1,18 @@
+package com.practice.cafe.dto.request.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String email;
+    private String password;
+
+    @Builder
+    public LoginRequestDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
