cfebf4c4312cd3275f2290ad8d3e3aae7b51448a
HyeonJin Song <shwj1602@naver.com>
2024-06-17 22:14:58 +0900

    Feat : JWT 인증 필터 추가

--- src/main/java/com/practice/cafe/config/WebSecurityConfig.java
+++ src/main/java/com/practice/cafe/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.practice.cafe.config;
 
+import com.practice.cafe.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -33,6 +37,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/cafe","/cafe/users/**").permitAll()
                         .anyRequest().authenticated()
                 );
+        httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
