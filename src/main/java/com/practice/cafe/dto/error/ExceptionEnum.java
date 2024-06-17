1d183d4be0601c2a4b0d8c8a548653a22eea34c4
HyeonJin Song <shwj1602@naver.com>
2024-06-17 22:14:09 +0900

    Feat : 예외 처리 코드 추가

--- src/main/java/com/practice/cafe/dto/error/ExceptionEnum.java
+++ src/main/java/com/practice/cafe/dto/error/ExceptionEnum.java
@@ -6,8 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionEnum {
     USER_DUPLICATION(HttpStatus.BAD_REQUEST.value(), "이미 사용 중인 이메일입니다."),
-    NOT_ADMIN_KEY(HttpStatus.BAD_REQUEST.value(), "관리자 키가 일치하지 않습니다.");
-
+    NOT_ADMIN_KEY(HttpStatus.BAD_REQUEST.value(), "관리자 키가 일치하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "사용자를 찾을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST.value(), "잘못된 비밀번호입니다.");
 
     private final int status;
     private final String message;
